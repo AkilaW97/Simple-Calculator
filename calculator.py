5ec4d896492231e3743e8d25159e6c8d1c77ea3a
Akila <woofyatw@gmail.com>
2025-02-25 19:38:44 +0530

    Added basic calculator functions and Main menu

---
+++ calculator.py
@@ -0,0 +1,22 @@
+#Calculation functions
+
+def add(x, y):
+    return x + y
+
+def subtract(x, y):
+    return  x - y
+
+def multiply(x, y):
+    return x * y
+
+def divide(x, y):
+    if y == 0:
+        return "Error! Division by zero"
+    return x / y
+
+#Main program
+print("Select operation")
+print("1. Add")
+print("2. Subtract")
+print("3. Multiply")
+print("4. Divide")
